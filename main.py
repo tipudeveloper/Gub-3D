4e6d594baabf3a87979c17286354b82b97a61b96
Tipudeveloper <lassi.palola@student.kokkola.fi>
2025-09-18 20:10:04 +0300

    add map

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import pygame as pg
 import sys
 from settings import *
+from map import Map
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.new_game()
     def new_game(self):
-        pass
+        self.map = Map(self)
     def update(self):
         pg.display.flip()
         self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps():.1f}')
     def draw(self):
         self.screen.fill('black')
+        self.map.draw()
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
