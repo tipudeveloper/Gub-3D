2ba27a5bf30e983d515c3c9ec15198199239fa06
Tipudeveloper <lassi.palola@student.kokkola.fi>
2025-09-19 08:57:17 +0300

    add raycasting

--- player.py
+++ player.py
@@ -42,9 +42,9 @@ class Player:
         if self.check_wall(int(self.x), int(self.y + dy)):
             self.y += dy
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
-                    (self.x * 100 + WIDTH * math.cos(self.angle),
-                    self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        #pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+        #            (self.x * 100 + WIDTH * math.cos(self.angle),
+        #            self.y * 100 + WIDTH * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
     def update(self):
         self.movement()
