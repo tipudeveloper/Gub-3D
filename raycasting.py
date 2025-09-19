21bf33803d7a4cd4f43c9515a17c44e4c101fc25
Tipudeveloper <lassi.palola@student.kokkola.fi>
2025-09-19 15:33:35 +0300

    fix fishbowl

--- raycasting.py
+++ raycasting.py
@@ -55,6 +55,9 @@ class RayCasting:
                 depth = depth_vert
             else:
                 depth = depth_hor
+
+            # remove fishbowl effect
+            depth *= math.cos(self.game.player.angle - ray_angle)
             
             # projection
             proj_height = SCREEN_DIST / (depth + 0.0001)
